0b52f24754a74648aab9b9df1d75d70cb8d198e3
Isaque Melo <isaquegbmelo2@gmail.com>
2019-03-14 18:29:20 -0300

    entrando na plataforma e saindo em utilizar move timed funcionou

--- main.py
+++ main.py
@@ -175,7 +175,7 @@ def return_last_color(robot, square_color, last_choice):
 robot = Robot()
 
 client = mqtt.Client()
-client.connect("169.254.75.196", 1883, 60)
+client.connect("169.254.125.180", 1883, 60)
 
 
 def on_message(client, userdata, message):
--- rescue.py
+++ rescue.py
@@ -55,11 +55,10 @@ def drop_doll(robot, speed=DEFAULT_SPEED):
         robot.dor_open = False
         robot.has_doll = False
 
-
 def bounding_box(robot, speed=DEFAULT_SPEED):
-
+    black_counter = 0
     robot.motors.alternative.run_timed(time_sp=1000, speed_sp=1000)
-
+    p_reverse = False
     # limits
     pid = PID(20, 0.2, 60.1, setpoint=83.3)
 
@@ -69,35 +68,52 @@ def bounding_box(robot, speed=DEFAULT_SPEED):
     pid.output_limits = (-400, 400)
     has_seen_black = False
     while True:
-        print("Bouding box loop..")
+        #print("Bouding box loop..")
         search = robot.sensor_data("ColorSensor")
+        if search[0] == "Black" and search[1] == "Black":
+            black_counter += 1
+            if black_counter >= 60:
+                drop_doll(robot)
+                # move back with pid
+                p_reverse = True
+                black_counter = 0
+
+        if p_reverse == True and "White" in search:
+            p_reverse = False
+            robot.rotate(180)
+            pid = PID(20, 0.2, 60.1, setpoint=75)
+            print(75)
+
         control = pid(robot.sensor_data("Ultrasonic"))
         ultrasonico = robot.sensor_data("Ultrasonic")
 
-        if search[0] == "Black" and search[1] == "Black" and not has_seen_black:
-
-            robot.move_timed(how_long=3.3, direction="forward", speed=speed)
-            drop_doll(robot)
-            robot.move_timed(how_long=1.6, direction="back", speed=speed)
-            robot.rotate(180)
-            robot.reverse_path = True
-            pid = PID(20, 0.2, 60.1, setpoint=75)
-
-            ev3.Sound.beep()
-            has_seen_black = True
-            #return
+        # if search[0] == "Black" and search[1] == "Black" and not has_seen_black:
+        #
+        #     robot.move_timed(how_long=3.3, direction="forward", speed=speed)
+        #     drop_doll(robot)
+        #     robot.move_timed(how_long=1.6, direction="back", speed=speed)
+        #     robot.rotate(180)
+        #     robot.reverse_path = True
+        #     pid = PID(20, 0.2, 60.1, setpoint=75)
+        #
+        #     ev3.Sound.beep()
+        #     has_seen_black = True
+        #     #return
 
 
         n_speed = 350
-        print("ULTRASONICO: ", ultrasonico)
+        #print("ULTRASONICO: ", ultrasonico)
         if ultrasonico >= 60 and ultrasonico <= 100:
             if control > 500:
                 control = 500
             if control < -500:
                 control = -500
-
-            robot.motors.left.run_forever(speed_sp=n_speed + control)
-            robot.motors.right.run_forever(speed_sp=n_speed - control)
+            if p_reverse == False:
+                robot.motors.left.run_forever(speed_sp=n_speed + control)
+                robot.motors.right.run_forever(speed_sp=n_speed - control)
+            else:
+                robot.motors.left.run_forever(speed_sp=(n_speed + control) * (-999/(n_speed + control)))
+                robot.motors.right.run_forever(speed_sp=(n_speed - control) * (-999/(n_speed - control)))
         else:
             robot.motors.left.run_forever(speed_sp=n_speed)
             robot.motors.right.run_forever(speed_sp=n_speed)
